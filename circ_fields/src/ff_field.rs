3168e084ac624b397e46fc7247fd88871562195d
Kunming Jiang <kunmingj@andrew.cmu.edu>
2023-12-13 11:00:56 -0800

    Finished adding ff

--- circ_fields/src/ff_field.rs
+++ circ_fields/src/ff_field.rs
@@ -20,7 +20,7 @@ macro_rules! def_field {
         pub mod $name {
             #![allow(warnings, clippy::derive_hash_xor_eq)]
             use datasize::DataSize;
-            use ff_derive::PrimeField;
+            use ff::PrimeField;
             use ff_derive_num::Num;
             use serde::{Deserialize, Serialize};
             #[derive(PrimeField, Num, Deserialize, Serialize, Hash, DataSize)]

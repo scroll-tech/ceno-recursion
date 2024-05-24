7a7821b6870236e41b9a1243ec7b70558a03904e
Kunming Jiang <kunmingj@andrew.cmu.edu>
2024-05-24 12:50:22 -0700

    Basic tester

---
+++ tester.py
@@ -0,0 +1,18 @@
+import os
+
+BENCHMARK = "2pc_if_return_cc"
+
+print("\n--\nCOBBL")
+os.system(f"cd circ_blocks && target/release/examples/zxc {BENCHMARK} | \
+          sed -n 's/Compiler time: //p'")
+os.system(f"cd spartan_parallel && RUST_BACKTRACE=1 target/release/examples/interface {BENCHMARK} | \
+            sed -n -e 's/Preprocess time: //p' \
+                -e 's/  \* SNARK::prove //p' \
+                -e 's/  \* SNARK::verify //p'")
+
+print("\n--\nBASELINE")
+os.system(f"cd circ_baseline && target/release/examples/circ --language zsharp {BENCHMARK} r1cs | \
+            sed -n -e 's/Compiler time: //p' \
+                -e 's/  \* SNARK::encode //p' \
+                -e 's/  \* SNARK::prove //p' \
+                -e 's/  \* SNARK::verify //p'")

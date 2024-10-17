894a619a6e4e70fe92a5e7194c92d50d9b006e61
Matthias Goergens <matthias.goergens@gmail.com>
2024-10-17 09:10:15 +0800

    Clean up setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-cd ./circ_blocks && cargo build --release --example zxc --no-default-features --features r1cs,smt,zok &&
-cd ../spartan_parallel && RUSTFLAGS="-C target_cpu=native" cargo build --release --features multicore,profile --example interface
+set -euxo pipefail
+
+(
+  cd circ_blocks
+  cargo build --release --example zxc --no-default-features --features r1cs,smt,zok
+)
+(
+  cd spartan_parallel
+  RUSTFLAGS="-C target_cpu=native" cargo build --release --features multicore,profile --example interface
+)

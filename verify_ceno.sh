45ef362b7a8c144190523d72386b10d4a0ae7ef8
Matthias Görgens <matthias.goergens@gmail.com>
2024-11-18 11:54:54 +0800

    Restore missing features (#26)

--- verify_ceno.sh
+++ verify_ceno.sh
@@ -3,5 +3,5 @@ set -euxo pipefail
 
 cd spartan_parallel
 RUST_BACKTRACE=1 \
-    cargo run --release \
+    cargo run --release --features multicore,profile \
         --example interface -- ceno_demo/tower_verifier
